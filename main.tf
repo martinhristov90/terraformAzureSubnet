37062516d7f08f9a9b0a88e3ee4ce861670f4ef6
Martin Hristov <martinhristov@Martins-MacBook-Pro.local>
2019-06-04 11:43:14 +0300

    Using terraform fmt

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 # Creating resource group for the resources.
 resource "azurerm_resource_group" "trainingRG" {
-    name        = "${var.RGname}"
-    location    = "${var.location}"
-    # Tagging for easier search.
-    tags {
-        environment = "trainingENV"
-    }
-  
+  name     = "${var.RGname}"
+  location = "${var.location}"
+
+  # Tagging for easier search.
+  tags {
+    environment = "trainingENV"
+  }
 }
 
 resource "azurerm_virtual_network" "trainingNetwork" {
-  name                  = "${azurerm_resource_group.trainingRG.name}-trainingNetwork"
-  address_space         = ["10.0.0.0/16"]
-  location              = "${azurerm_resource_group.trainingRG.location}"
-  resource_group_name   = "${azurerm_resource_group.trainingRG.name}"
-  
+  name                = "${azurerm_resource_group.trainingRG.name}-trainingNetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.trainingRG.location}"
+  resource_group_name = "${azurerm_resource_group.trainingRG.name}"
+
   subnet {
-      name              = "${azurerm_resource_group.trainingRG.name}-trainingSubnet"
-      address_prefix    = "10.0.1.0/24"
+    name           = "${azurerm_resource_group.trainingRG.name}-trainingSubnet"
+    address_prefix = "10.0.1.0/24"
   }
 
   # Tagging for easier search.
   tags {
-      environment       = "trainingENV"
+    environment = "trainingENV"
   }
 }
